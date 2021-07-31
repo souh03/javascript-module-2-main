579bf3631a98775701e8e32dd1f7083e9f8351a3
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-07-31 10:35:34 +0200

    Add files via upload

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,4 +1,4 @@
-  function myFunction() {
+  function FunctionBL() {
 	var elements = document.getElementsByClassName('jumbotron'); 
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "#588fbd";
@@ -10,7 +10,44 @@
     
     var elements = document.getElementsByClassName('btn btn-secondary btn-lrg'); 
 	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor =  "black";
+		elements[i].style.color =  "white";
+		
+	}
+
+}
+function FunctionOr() {
+	var elements = document.getElementsByClassName('jumbotron'); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#f0ad4e";
+	}
+    var elements = document.getElementsByClassName('btn btn-primary btn-lrg'); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#5751fd";
+	}
+    
+    var elements = document.getElementsByClassName('btn btn-secondary btn-lrg'); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#31b0d5";
+		
+	}
+
+}
+function FunctionGr() {
+	var elements = document.getElementsByClassName('jumbotron'); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#87ca8a";
+	}
+    var elements = document.getElementsByClassName('btn btn-primary btn-lrg'); 
+	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "black";
+		
+	}
+    
+    var elements = document.getElementsByClassName('btn btn-secondary btn-lrg'); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#8c9c08";
+		
 	}
 
 }
